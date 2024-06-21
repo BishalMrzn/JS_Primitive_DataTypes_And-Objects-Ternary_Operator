289d573b298d77dc4c09b1aca92c61b1300a9f61
BishalMrzn <112357355+BishalMrzn@users.noreply.github.com>
2024-06-21 11:47:25 +0545

    Add files via upload

---
+++ Primitivedt and object/main.js
@@ -0,0 +1,20 @@
+// nn ss bb u
+let a =600
+let b =null;
+let c =true //can also be false
+let d =BigInt("23434")+BigInt("2")
+let e=Symbol("This is a symbol")
+let f="yo"
+let g =undefined
+console.log(a,b,c,d,e,f,g)
+
+const room={
+    "Bishal":"Room 1",
+    "Sarin":"Room 5",
+    "Mahendra":"Room 4",
+    "Ranjit":"Room 3",
+    "Seshan":"Room 2"
+}
+console.log(typeof room)
+console.log(room)
+console.log(room["Mahendra"])
---
+++ Ternary/main.js
@@ -0,0 +1,11 @@
+let age=16
+// let drink=(age>=18)?"beer":"juice";
+// console.log(drink)
+let drink;
+if ((age<=18)){
+    drink="Juice"
+}
+else{
+    drink="beer"
+}
+console.log(drink)
